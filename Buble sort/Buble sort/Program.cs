cc38624398203d1eee02d98e809dfc58dc3abfbc
Ivan <ragna.blood82@gmail.com>
2023-09-25 14:29:13 +0300

    Buble sort (1)

---
+++ Buble sort/Buble sort/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] numbers = new int[42];
+Random rand = new Random();
+for (int i = 0; i < numbers.Length; i++)
+    numbers[i] = rand.Next();
+
+int temp;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers.Length - 1 - i; j++)
+    {
+        if (numbers[j] > numbers[j + 1])
+        {
+            temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+    }
+
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
